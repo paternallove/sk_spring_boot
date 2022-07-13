783677337601cc284c9e2f5063b5098aa0ad81b0
kwon-mesh <kwon.son@meshkorea.net>
2022-07-13 10:43:22 +0900

    initialize

---
+++ src/main/java/com/example/sk_spring_boot/SkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sk_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkSpringBootApplication.class, args);
+    }
+
+}
